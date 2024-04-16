1b16c569776e37183c7656791497aba577102fb4
JoaoGuiNovaes <joao.novaes@aluno.ifsp.edu.br>
2024-04-16 17:04:47 -0300

    Animações

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    private Rigidbody2D playerRigidBody2D;
+   private Animator playerAnimator;
    public  float playerSpeed;
    private Vector2 playerDirection;
 
     void Start()
-    {
-       playerRigidBody2D = GetComponent<Rigidbody2D>();
+    {   
+        playerAnimator = GetComponent<Animator>();
+        playerRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(playerDirection.sqrMagnitude > 0)
+        {
+            playerAnimator.SetInteger("Movimento", 1);
+        } else{
+            playerAnimator.SetInteger("Movimento", 0);
+        }
+
+        Flip();
+
     }
 
     void FixedUpdate(){
         playerRigidBody2D.MovePosition(playerRigidBody2D.position + playerDirection * playerSpeed * Time.fixedDeltaTime);
     }
 
+ void Flip(){
+        if(playerDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        } 
+        else if (playerDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
 }
+
+   
