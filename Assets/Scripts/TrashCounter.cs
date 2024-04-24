c16b104ab2387a387bec0272f877ed42e32e202a
JoaoGuiNovaes <joao.novaes@aluno.ifsp.edu.br>
2024-04-24 19:27:53 -0300

    Ta contando os lixo finalmente

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrashCounter : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI trashCounterText; // Referência ao objeto de texto na UI
+      public string[] tagsToCheck = { "MetalTrash", "GlassTrash"};  // Array de tags para verificar
+
+    void UpdateTrashCounter()
+{
+
+    int totalTrashCount = 0;
+
+    foreach (string tag in tagsToCheck)
+    {
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(tag);
+        totalTrashCount += objectsWithTag.Length;
+    }
+        trashCounterText.text = "Lixos restantes: " + totalTrashCount;
+        Debug.Log("Total de lixos restantes: " + totalTrashCount);
+}
+    void Update()
+    {
+        UpdateTrashCounter(); // Atualiza o contador a cada frame (ou em intervalos específicos)
+    }
+}
