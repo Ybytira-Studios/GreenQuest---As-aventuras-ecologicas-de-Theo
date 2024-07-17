f07f48f751dc5552512539a2b3626b496c1da72c
João Guilherme <joao.novaes@aluno.ifsp.edu.br>
2024-07-17 13:51:34 -0300

    Primeiro menu inicial + sistema de volume
    Tem um pequeno "bug" que quando voce reinicia a cena o mesmo com o volume zerado o sprite não volta com o a imagem de mute

---
+++ Assets/Scripts/MainInterface.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainInterface : MonoBehaviour
+{
+    public GameObject OptionMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play(){
+        SceneManager.LoadScene("Fase1_beach");
+    }
+
+    public void Options(){
+        OptionMenu.SetActive(true);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+
+}
